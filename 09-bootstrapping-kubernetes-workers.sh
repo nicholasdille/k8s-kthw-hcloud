777b70510285f55902ee6e8db02899636688fc81
Nicholas Dille <nicholas.dille@mailbox.org>
2019-01-26 00:32:41 +0100

    Implementation of kubenet with containerd

--- 09-bootstrapping-kubernetes-workers.sh
+++ 09-bootstrapping-kubernetes-workers.sh
@@ -24,6 +24,7 @@ POD_CIDR=10.200.0.0/16
 echo "============== Download and Install Worker Binaries"
 wget -q --show-progress --https-only --timestamping \
   https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.12.0/crictl-v1.12.0-linux-amd64.tar.gz \
+  https://storage.googleapis.com/kubernetes-the-hard-way/runsc-50c283b9f56bb7200938d9e207355f05f79f0d17 \
   https://github.com/opencontainers/runc/releases/download/v1.0.0-rc5/runc.amd64 \
   https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-amd64-v0.6.0.tgz \
   https://github.com/containerd/containerd/releases/download/v1.2.0-rc.0/containerd-1.2.0-rc.0.linux-amd64.tar.gz \
@@ -42,9 +43,10 @@ sudo mkdir -p \
 
 echo "============== Install the worker binaries:"
 {
+  sudo mv runsc-50c283b9f56bb7200938d9e207355f05f79f0d17 runsc
   sudo mv runc.amd64 runc
-  chmod +x kubectl kube-proxy kubelet runc
-  sudo mv kubectl kube-proxy kubelet runc /usr/local/bin/
+  chmod +x kubectl kube-proxy kubelet runc runsc
+  sudo mv kubectl kube-proxy kubelet runc runsc /usr/local/bin/
   sudo tar -xvf crictl-v1.12.0-linux-amd64.tar.gz -C /usr/local/bin/
   sudo tar -xvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin/
   sudo tar -xvf containerd-1.2.0-rc.0.linux-amd64.tar.gz -C /
@@ -169,8 +171,7 @@ ExecStart=/usr/local/bin/kubelet \\
   --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \\
   --image-pull-progress-deadline=2m \\
   --kubeconfig=/var/lib/kubelet/kubeconfig \\
-  --network-plugin=kubenet \\
-  --non-masquerade-cidr=${POD_CIDR} \\
+  --network-plugin=cni \\
   --register-node=true \\
   --v=2
 Restart=on-failure
